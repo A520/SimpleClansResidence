e09be35679441483052e7878d4ece6a78d078747
toxuin <otstoypuksa@ya.ru>
2012-07-11 16:46:32 +0400

    Real initial commit. ver 0.1.10

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 ResidenceSimpleClans
 ====================
 
-A bridge between Residence and SimpleClans [minecraft bukkit plugins]
+This plugin is a bridge between [Residence](http://dev.bukkit.org/server-mods/residence/) and [SimpleClans](http://dev.bukkit.org/server-mods/simpleclans/).
+
+It adds 4 new flags to any residence:
+
+* clanbuild - to allow clanners to build on your land
+
+* clanuse - to allow your clanmates to use stuff inside your house
+
+* clanmove - to admit only members of your clan to your party (falls back to move flag!)
+
+* clancontainer - to make a shared storage for your clan(?)
+
+**INSTALLATION**
+
+You have to edit your Residence's config.yml (it's in your plugins\Residence folder). You have to add all these flags to Global (default: true), FlagPermission (default: true) and ResidenceDefault (default: false) sections. After that just restart your server and go add some flags. No commands, no config, just that simple.
+
+**REQUIREMENTS**
+
+This plugin DOES REQUIRE both [Residence](http://dev.bukkit.org/server-mods/residence/) AND [SimpleClans](http://dev.bukkit.org/server-mods/simpleclans/). If you don't have one of this set - it not gonna work.
+
+Bukkit version 1.1R1 is minimum required, but this plugin works on most recent build also without any problems.
+
+**THANKS**
+
+Thanks to authors of Residence, especially *bekvon*. Without his advise this plugin would never work as intended.
---
+++ src/ru/obsidian_mc/toxuin/SimpleClansResidence.java
@@ -0,0 +1,110 @@
+package ru.obsidian_mc.toxuin;
+
+import java.util.logging.Logger;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.sacredlabyrinth.phaed.simpleclans.ClanPlayer;
+import net.sacredlabyrinth.phaed.simpleclans.SimpleClans;
+
+import com.bekvon.bukkit.residence.event.ResidenceFlagCheckEvent;
+import com.bekvon.bukkit.residence.protection.FlagPermissions;
+
+public class SimpleClansResidence extends JavaPlugin implements Listener {
+	public static String prefix = null;
+	public static Logger log = Logger.getLogger("Minecraft");
+	private SimpleClans sc;
+
+	@Override
+	public void onEnable(){
+		prefix = "[" + this.getDescription().getName()+ "]: ";
+		this.getServer().getPluginManager().registerEvents(this, this);
+		
+		// ADD NEW FLAG
+		if (this.getServer().getPluginManager().isPluginEnabled("Residence")) {
+			FlagPermissions.addResidenceOnlyFlag("clanmove");
+			FlagPermissions.addResidenceOnlyFlag("clanbuild");
+			FlagPermissions.addResidenceOnlyFlag("clanuse");
+			FlagPermissions.addResidenceOnlyFlag("clancontainer");
+		} else {
+			log.info(prefix+"ERROR: Residence NOT FOUND!!!");
+		}
+		
+		Plugin plug = getServer().getPluginManager().getPlugin("SimpleClans");
+	    if (plug != null) sc = ((SimpleClans) plug); else log.info(prefix+"ERROR: SimpleClans NOT FOUND!!!");
+	    
+	}
+	
+	@Override
+	public void onDisable(){
+		sc = null;
+		log.info( prefix + "Disabled.");
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+	public void onResidenceFlagCheck(ResidenceFlagCheckEvent event) {
+		if (event.getFlagTargetPlayerOrGroup() == null) return;
+		if (event.getResidence().getOwner().equals(event.getFlagTargetPlayerOrGroup())) return;
+		
+		if (event.getFlag().equals("build")) {
+			if (!event.getResidence().getPermissions().has("clanbuild", false)) return;
+			
+			ClanPlayer hostClanPlayer = sc.getClanManager().getClanPlayer(event.getResidence().getOwner());
+			if (hostClanPlayer==null) return;
+			
+			ClanPlayer visitorClanPlayer = sc.getClanManager().getClanPlayer(event.getFlagTargetPlayerOrGroup());
+			if (visitorClanPlayer==null) return;
+			
+			if (hostClanPlayer !=null && visitorClanPlayer != null && visitorClanPlayer.getClan().equals(hostClanPlayer.getClan())) {
+				event.override = true;
+				event.overridevalue = true;
+			}
+			
+		} else if (event.getFlag().equals("use")) {
+			if (!event.getResidence().getPermissions().has("clanuse", false)) return;
+			
+			ClanPlayer hostClanPlayer = sc.getClanManager().getClanPlayer(event.getResidence().getOwner());
+			if (hostClanPlayer==null) return;
+			
+			ClanPlayer visitorClanPlayer = sc.getClanManager().getClanPlayer(event.getFlagTargetPlayerOrGroup());
+			if (visitorClanPlayer==null) return;
+			
+			if (hostClanPlayer !=null && visitorClanPlayer != null && visitorClanPlayer.getClan().equals(hostClanPlayer.getClan())) {
+				event.override = true;
+				event.overridevalue = true;
+			}
+		} else if (event.getFlag().equals("move")) {
+			if (!event.getResidence().getPermissions().has("clanmove", false)) return;
+			
+			ClanPlayer hostClanPlayer = sc.getClanManager().getClanPlayer(event.getResidence().getOwner());
+			if (hostClanPlayer==null) return;
+			
+			ClanPlayer visitorClanPlayer = sc.getClanManager().getClanPlayer(event.getFlagTargetPlayerOrGroup());
+			if (visitorClanPlayer==null) return;
+			
+			if (hostClanPlayer !=null && visitorClanPlayer != null && visitorClanPlayer.getClan().equals(hostClanPlayer.getClan())) {
+				event.override = true;
+				event.overridevalue = true;
+			}
+			
+		} else if (event.getFlag().equals("container")) {
+			if (!event.getResidence().getPermissions().has("clancontainer", false)) return;
+			
+			ClanPlayer hostClanPlayer = sc.getClanManager().getClanPlayer(event.getResidence().getOwner());
+			if (hostClanPlayer==null) return;
+			
+			ClanPlayer visitorClanPlayer = sc.getClanManager().getClanPlayer(event.getFlagTargetPlayerOrGroup());
+			if (visitorClanPlayer==null) return;
+			
+			if (hostClanPlayer !=null && visitorClanPlayer != null && visitorClanPlayer.getClan().equals(hostClanPlayer.getClan())) {
+				event.override = true;
+				event.overridevalue = true;
+			}
+			
+		}
+	}
+}
